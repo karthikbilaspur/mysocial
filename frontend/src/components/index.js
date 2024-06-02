1ba355583d72c1c6562e25cba3bba1b81fbbc339
V.Karthik <36740831+karthikbilaspur@users.noreply.github.com>
2024-06-02 14:19:04 +0530

    commit

---
+++ frontend/src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Feed } from './Feed';
+export { default as Details } from './Details';
+export { default as UserProfile } from './UserProfile';
+export { default as CreatePin } from './CreatePin';
+export { default as Search } from './Search';
+export { default as Login } from './Login';
+export { default as Layout } from './Layout';
